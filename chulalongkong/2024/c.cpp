4c966a96947c26daae6ca71917c0655ce1d1370e
shinchan <32698751+shinchankosen@users.noreply.github.com>
2025-10-03 01:15:10 +0900

    Add C++ program to calculate product of two largest numbers
    Implement a C++ program to read integers, sort them, and output the product of the two largest numbers.

---
+++ chulalongkong/2024/c.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define all(v) (v).begin(),(v).end()
+#define pb(a) push_back(a)
+#define rep(i, n) for(int i=0;i<n;i++)
+#define foa(e, v) for(auto&& e : v)
+using ll = long long;
+const ll MOD7 = 1000000007, MOD998 = 998244353, INF = (1LL << 60);
+#define dout(a) cout<<fixed<<setprecision(10)<<a<<endl;
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    rep(i, n) cin >> a[i];
+    sort(all(a));
+    reverse(all(a));
+    cout << a[0] * a[1] << endl;
+    return 0;
+}
